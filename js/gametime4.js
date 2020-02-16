2660989dd2703466b463c901123b61011f41331f
= <postoffice@londonbusred.co.uk>
2020-02-16 15:25:23 +0000

    timer refinements

--- js/gametime4.js
+++ js/gametime4.js
@@ -1,33 +1,53 @@
 let controls = document.getElementById('controls');
 let gametime = document.getElementById('gametime');
 let intermission = document.getElementById('intermission');
-let min, sec, int;
+let demo = document.getElementById('demo');
+let min, sec, int, warn;
 
 function startpause(i)
 {
   console.log("click", i.id);
   controls.style.display = 'none';
+  demo.style.display = 'none';
   min = 0; // available options return seconds only
   sec = gametime.value;
   int = intermission.value;
-  countdown(i.id, min, sec, int);
+  warn = 60;
+  countdown(i.id, min, sec, int, warn);
 }
 
-function countdown(element, minutes, seconds, intermission)
+function demonstration()
 {
-  console.log(element, minutes, seconds, intermission);
+  console.log("start demo");
+  controls.style.display = 'none';
+  demo.style.display = 'none';
+  min = 0; // available options return seconds only
+  sec = 10;
+  int = 2;
+  warn = 3;
+  countdown('timer', min, sec, int, warn);
+}
+
+function countdown(element, minutes, seconds, intermission, warning)
+{
+  console.log(element, minutes, seconds, intermission, warning);
   let time = minutes * 60 + seconds;
   let el = document.getElementById(element);
   let interval = setInterval(function() {
-    if (time == 0)
+    // start or restart the timer
+    if (time < 0)
     {
-      let text = 'Change!';
+      let text = 'Change';
       el.innerHTML = text;
       setTimeout(function() {
-        countdown('timer', min, sec);
+        countdown('timer', min, sec, int, warn);
       }, intermission * 1000);
       clearInterval(interval);
       return;
+    }
+    else if (time == warning)
+    {
+      console.log("Warning!");
     }
     let minutes = Math.floor(time / 60);
     if (minutes < 10) minutes = '0' + minutes;
